c3e2ed23936b39caa51293a2d145eb46da99b42e
CESBEAM <softwareversion2.8@gmail.com>
2022-01-06 13:51:50 +0800

    repath the files only

---
+++ Lessons/monthSale.php
@@ -0,0 +1,68 @@
+<?php
+//input by users
+$day1= 440;
+$day2= 240;
+$day3= 140;
+$day4= 540;
+$day5= 840;
+$day6= 740;
+$day7= 540;
+
+//add the days to total the week sales
+// $totalWeek = $day1+$day2+$day3+$day4+$day5+$day6+$day7;
+
+// echo"total week sales: ".$totalWeek;
+
+echo"<br><br>";
+
+//array
+// $weekSale = array($day1+$day2+$day3+$day4+$day5+$day6+$day7);
+
+
+// print_r($weekSale);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
+// $day1 = 450;
