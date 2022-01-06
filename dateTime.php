bbd766d5a680452370988897e2001b20551236cf
CESBEAM <softwareversion2.8@gmail.com>
2022-01-06 11:32:29 +0800

    dateTime

--- dateTime.php
+++ dateTime.php
@@ -2,7 +2,17 @@
 
 date_default_timezone_set('Asia/Manila');
 echo "<br/>";
-$date = 'January 05, 2022 07:16:00 PM';
-echo $date;
+// $date = 'January 05, 2022 07:16:00 PM';
+// echo $date;
 echo "<br>";
-echo date('m/d/y <br/>H:i:s a',strtotime($date));
+echo date('m/d/y <br/>H:i:s a');
+
+echo "<br><br>";
+
+
+//new line
+date_default_timezone_set('Asia/Manila');
+
+echo date('M / D / Y ');
+echo "<br>";
+echo date('h:i:s a');
